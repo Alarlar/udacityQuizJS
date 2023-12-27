f2475d98341cf51f2b805b4c083f7053ab5026c1
Alarlar <52812499+Alarlar@users.noreply.github.com>
2023-12-26 19:21:21 -0800

    celcsiusToFahrenheit

---
+++ celcsiusToFahrenheit.js
@@ -0,0 +1,12 @@
+To convert Celsius to Fahrenheit, you can use the following formula:
+
+F=C×1.8+32
+
+Code:
+var celsius = 12;
+var fahrenheit = celsius* 1.8 + 32;
+
+console.log(fahrenheit);
+
+Output:
+53.6
