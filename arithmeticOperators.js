77c36b3c8a8b130e60e7e02a7c0b4a192777b432
Alarlar <52812499+Alarlar@users.noreply.github.com>
2023-12-27 20:19:12 -0800

    arithmeticOperators

---
+++ arithmeticOperators.js
@@ -0,0 +1,9 @@
+/*Quiz-1
+Write an expression that uses at least 3 different arithmetic operators.
+The expression should equal 42.
+Hint: +, -, *, /, and % are possible arithmetic operators
+
+Code:
+this expression equals 4, change it to equal 42
+*/
+console.log(1 + 5 - 2 + 38);
