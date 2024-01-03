2fdf493481871feff21df3770010d772de0487ef
Alarlar <52812499+Alarlar@users.noreply.github.com>
2024-01-02 20:33:31 -0800

    notAnumber

---
+++ notAnumber.js
@@ -0,0 +1,7 @@
+//What will be printed out?
+
+var signedIn;
+console.log(signedIn);
+
+// undefined 
+/*The variable signedIn was never assigned a value, so it will print out undefined. It's worth noting that signedIn could explicitly be assigned the value of null.*/
